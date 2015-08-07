7709f6da63fc20a6918906ee1a9ec780a4f164f1
Ognian Chirkov <ognian@falconsocial.com>
2015-08-07 16:53:34 +0200

    pretty print timestamp as plain string

--- marketo/wrapper/lead_activity.py
+++ marketo/wrapper/lead_activity.py
@@ -18,7 +18,7 @@ class LeadActivity:
 def unwrap(xml):
     activity = LeadActivity()
     activity.id = xml.find('id').text
-    activity.timestamp = iso8601.parse_date(xml.find('activityDateTime').text)
+    activity.timestamp = (iso8601.parse_date(xml.find('activityDateTime').text)).strftime("%d-%b-%Y %H:%m")
     activity.type = xml.find('activityType').text
     activity.assetname = xml.find('mktgAssetName').text
     activity.campaign = xml.find('campaign').text
