32919c50d447cbe10816ba04c11cc0dca1ad5696
Dmitry Kalashnikov <dmitry@calibermind.com>
2018-12-10 14:41:03 +0300

    headers upd and python 3 upd

--- marketo/__init__.py
+++ marketo/__init__.py
@@ -17,7 +17,7 @@ class Client:
         if not soap_endpoint or not isinstance(soap_endpoint, str):
             raise ValueError('Must supply a soap_endpoint as a non empty string.')
 
-        if not user_id or not isinstance(user_id, (str, unicode)):
+        if not user_id or not isinstance(user_id, str):
             raise ValueError('Must supply a user_id as a non empty string.')
 
         if not encryption_key or not isinstance(encryption_key, str):
@@ -30,18 +30,14 @@ class Client:
     def wrap(self, body):
         return (
             '<?xml version="1.0" encoding="UTF-8"?>' +
-            '<env:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' +
-                          'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
-                          'xmlns:wsdl="http://www.marketo.com/mktows/" ' +
-                          'xmlns:env="http://schemas.xmlsoap.org/soap/envelope/" ' +
-                          'xmlns:ins0="http://www.marketo.com/mktows/" ' +
+            '<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"' +
                           'xmlns:ns1="http://www.marketo.com/mktows/" ' +
-                          'xmlns:mkt="http://www.marketo.com/mktows/">' +
+                          'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '+
                 auth.header(self.user_id, self.encryption_key) +
-                '<env:Body>' +
+                '<SOAP-ENV:Body>' +
                     body +
-                '</env:Body>' +
-            '</env:Envelope>')
+                '</SOAP-ENV:Body>' +
+            '</SOAP-ENV:Envelope>')
 
     def request(self, body):
         envelope = self.wrap(body)
@@ -56,7 +52,7 @@ class Client:
 
     def get_lead(self, email=None):
 
-        if not email or not isinstance(email, (str, unicode)):
+        if not email or not isinstance(email, str):
             raise ValueError('Must supply an email as a non empty string.')
 
         body = get_lead.wrap(email)
@@ -68,7 +64,7 @@ class Client:
 
     def get_lead_activity(self, email=None, filters=[]):
 
-        if not email or not isinstance(email, (str, unicode)):
+        if not email or not isinstance(email, str):
             raise ValueError('Must supply an email as a non empty string.')
 
         body = get_lead_activity.wrap(email, filters)
@@ -80,10 +76,10 @@ class Client:
 
     def request_campaign(self, campaign=None, lead=None):
 
-        if not campaign or not isinstance(campaign, (str, unicode)):
+        if not campaign or not isinstance(campaign, str):
             raise ValueError('Must supply campaign id as a non empty string.')
 
-        if not lead or not isinstance(lead, (str, unicode)):
+        if not lead or not isinstance(lead, str):
             raise ValueError('Must supply lead id as a non empty string.')
 
         body = request_campaign.wrap(campaign, lead)
@@ -96,7 +92,7 @@ class Client:
 
     def sync_lead(self, email=None, attributes=None):
 
-        if not email or not isinstance(email, (str, unicode)):
+        if not email or not isinstance(email, str):
             raise ValueError('Must supply lead id as a non empty string.')
 
         if not attributes or not isinstance(attributes, tuple):
